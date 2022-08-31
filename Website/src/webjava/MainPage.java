d1482b9298bd44c464c4076922cfb28215820df1
classico09 <nguyenbuibaoduy@naver.com>
2022-08-31 12:57:46 +0900

    testing java in vscode

---
+++ Website/src/webjava/MainPage.java
@@ -0,0 +1,11 @@
+package webjava;
+
+public class MainPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String x = "hello";
+		System.out.println(x);
+	}
+
+}
