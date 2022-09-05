c792131763f6fa84c03b043e04aee6b0115b1cd0
trungtit <62939240+trungtit@users.noreply.github.com>
2022-09-05 17:28:36 +0900

    Update scripts.js

--- Website/src/main/webapp/js/scripts.js
+++ Website/src/main/webapp/js/scripts.js
@@ -1,7 +1,28 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.5 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+function validateForm(){
+	var name = document.forms["registration"]["full_name"].value;
+	var email = document.forms["registration"]["email"].value;
+	var pw = document.forms["registration"]["password"].value;
+	var confirmPw = document.forms["registration"]["confirm_password"].value;
+	var mobile = document.forms["registration"]["phone_number"].value;
+	
+	if(name==""){
+		alert("Name must be filled out!");
+		return false;
+	}
+	else if(email==""){
+		alert("Email address must be filled out!");
+		return false;	
+	}
+	else if(pw==""){
+		alert("Password must be filled out!");
+		return false;	
+	}
+	else if(confirmPw==""){
+		alert("Password must be confirmed!");
+		return false;	
+	}
+	else if(mobile==""){
+		alert("Phone number must be filled out!");
+		return false;	
+	}
+}
