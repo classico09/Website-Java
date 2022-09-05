c3d9cc880dafc22af4859f43b6246a349307ce32
trungtit <62939240+trungtit@users.noreply.github.com>
2022-09-05 22:14:45 +0900

    Update Register.java

--- Website/src/main/java/com/servlet/mainpage/Register.java
+++ Website/src/main/java/com/servlet/mainpage/Register.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.mysql.cj.xdevapi.PreparableStatement;
 
 @WebServlet("/register")
 public class Register extends HttpServlet {
