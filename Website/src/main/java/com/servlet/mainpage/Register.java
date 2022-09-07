1751d8c16648db957435200167ca4e74da4e9547
trungtit <62939240+trungtit@users.noreply.github.com>
2022-09-07 23:58:04 +0900

    Update Register.java

--- Website/src/main/java/com/servlet/mainpage/Register.java
+++ Website/src/main/java/com/servlet/mainpage/Register.java
@@ -1,82 +1,38 @@
-package com.servlet.mainpage;
+package sw.frontend;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import sw.entity.Account;
+import sw.repository.AccountRepository;
+import sw.ultis.ScannerUltis;
 
 
 @WebServlet("/register")
 public class Register extends HttpServlet {
-	
-	//create the query 
-	private static final String INSERT_QUERY = "INSERT INTO USER(userName, email, password, mobile) VALUES(?, ?, ?, ?)";
-	
-	
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//get PrintWriter
-		PrintWriter pw = resp.getWriter();
-		//set content type
-		resp.setContentType("text/html");
-		//read the form values
-		String name = req.getParameter("full_name");
-		String email = req.getParameter("email");
-		String password = req.getParameter("password");
-		String mobile = req.getParameter("phone_number");
-	
-//		System.out.println("Full name: "+name );
-//		System.out.println("Email: "+email);
-//		System.out.println("Pw : "+password);
-//		System.out.println("Phone number: "+ mobile);
-		
-		//Load the jdbc driver
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		//create the connection
-		try(Connection con = DriverManager.getConnection("jdbc:mysql://vteam8.cbr4uubmqr4e.ap-northeast-2.rds.amazonaws.com:3306/YouthDataHibernate","admin","HHzOAYbHqbL8q5M");
-			PreparedStatement ps = con.prepareStatement(INSERT_QUERY)) {
-			//set the values
-			ps.setString(1, name);
-			ps.setString(2, email);
-			ps.setString(3, password);
-			ps.setString(4, mobile);
-			
-			//execute the query
-			int count = ps.executeUpdate();
-			if(count == 0) {
-				pw.println("Registration failed /n Please, try again!");
-			}else {
-				pw.println("Success!");
-			}
-			
-		} catch (SQLException se) {
-			pw.println(se.getMessage());
-			se.printStackTrace();
-		}catch(Exception e){
-			pw.println(e.getMessage());
-			e.printStackTrace();
-		}
-		
-		//close the stream
-		pw.close();
-	}
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(req, resp);
-	}
+	private void doGet(HttpServletRequest req, HttpServletResponse res) {
 		
-}
+		PrintWriter pw = res.getWriter();
+		
+		res.setContentType("text/html");
+		
+		String email = req.getParameter("email");
+		String name = req.getParameter("full_name");
+		String password = req.getParameter("password");
+		String mobile = req.getParameter("mobile");
+		
+		Accout acc = new Account();
+		acc.setEmail(email); 
+		acc.setFullName(name); 
+		acc.setPassword(password); 
+		acc.setMobile(mobile); 
+		
+		AccountRepository accRepo = new AccountRepository();
+		accRepo.createAccount(acc);
+	}
+	private void doPost(HttpServletRequest req, HttpServletResponse res) {
+		doGet(req, res);
+
+	}
+	
+
